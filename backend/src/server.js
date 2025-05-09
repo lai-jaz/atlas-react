478d7dcc659940493dc929e908302220d2c374f2
Aleena <125188065+20alina03@users.noreply.github.com>
2025-05-10 00:36:42 +0500

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import locationRoutes from "./routes/locations.js";
 import tipRoutes from './routes/tips.js';
-import journalRoutes from "./routes/journalRoutes.js";
+import roammateRoutes from './routes/roammates.js';
+import journalRoutes form "./routes/journalRoutes.js";
 dotenv.config();
 const app = express();
 
@@ -14,9 +15,10 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api/locations", locationRoutes);
 app.use('/api/tips', tipRoutes);
-app.use("/api/journals",journalRoutes);
+app.use('/api/roammates', roammateRoutes);
+app.use('/api/journals', journalRoutes);
+
 mongoose
 .connect(process.env.MONGO_URI)
 .then(() => app.listen(3000, () => console.log("Server running")))
 .catch(console.error);
-
