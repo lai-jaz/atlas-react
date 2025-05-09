982b36acb20cfc4a3daf5b39e33e10b1f9ec8bd9
Fatima Khan <163330776+fatimaKhan003@users.noreply.github.com>
2025-05-09 21:37:27 +0500

    schema for journals

---
+++ backend/src/models/Journal.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const journalSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  excerpt: { type: String },
+  location: { type: String },
+  tags: [{ type: String }],
+  // imageUrl: { type: String },
+  author: {
+    name: { type: String },
+    // avatar: { type: String },
+  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },  // Link to the user
+  date: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('Journal', journalSchema);
