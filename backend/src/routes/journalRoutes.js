031f5e90ebbc296e907895848f7eb00f35d61535
Fatima Khan <163330776+fatimaKhan003@users.noreply.github.com>
2025-05-11 16:47:30 +0500

    Add files via upload

--- backend/src/routes/journalRoutes.js
+++ backend/src/routes/journalRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { getAllJournals, createJournal, getJournalById } from '../controller/journalController.js';
+import { getAllJournals, createJournal, 
+    getJournalById, deleteJournal,updateJournal,memory } from '../controller/journalController.js';
 const router = express.Router();
+router.get('/memories', memory);
 router.get('/',getAllJournals);
 router.post('/',createJournal);
 router.get('/:id',getJournalById);
+router.delete('/:id',deleteJournal);
+router.put('/:id',updateJournal);
+
+
 export default router;
